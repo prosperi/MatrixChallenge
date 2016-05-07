3ac483c91050b92ac00043adb7c1dfac518bed06
Zura <prosperrriii@gmail.com>
2016-05-07 11:28:40 +0400

    Finished parsing part

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
-var input = document.getElementsByName("system")[0];
+var system = document.getElementsByName("system")[0];
+var answer = document.getElementsByTagName("code")[1];
 
-console.log(input);
+function calculate(){
+
+  var variables = {},
+      exp,
+      equations = system.value.replace(/ /g, '').split('\n');
+  answer.textContent = "Answer: ";
+
+  for(var i = 0; i < system.value.length; i++){
+    if(/[a-z]/.test(system.value.charAt(i)) && !(system.value.charAt(i) in variables)){
+      variables[system.value.charAt(i)] = [];
+    }
+  }
+
+  exp = new RegExp("^((\\d*[a-z][\\+-])+\\d*[a-z]\\=\\d+,?){" + Object.keys(variables).length + "}");
+
+  if(exp.test(equations)){
+    console.log(variables, exp.exec(equations)[0]);
+    for(var i = 0; i < equations.length; i++){
+      for(var vrb in variables){
+        var vrbExp = new RegExp("(\\d*)" + vrb);
+
+        if(vrbExp.test(equations[i])){
+          var result = vrbExp.exec(equations[i])[1];
+          (result) ? variables[vrb].push(Number(result)) : variables[vrb].push(1);
+        }else{
+          variables[vrb].push(0);
+        }
+
+      }
+    }
+    console.log(variables);
+  }else{
+    answer.textContent += "boom";
+  }
+
+}
